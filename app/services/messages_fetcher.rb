2a8603e0ba8f52114821ba6160c5b230d79fa9ee
Dominik Bylica <byldominik+fenoloftaleina@gmail.com>
2016-02-20 00:05:30 +0100

    Add messages fetcher.

---
+++ app/services/messages_fetcher.rb
@@ -0,0 +1,20 @@
+class MessagesFetcher < Struct.new(:last_message_id)
+  RECENT_MESSAGES_AMOUNT = 30
+
+  def call
+    if !last_message_id
+      recent_messages
+    else
+      newer_messages
+    end
+  end
+
+  private
+  def recent_messages
+    Message.last(RECENT_MESSAGES_AMOUNT)
+  end
+
+  def newer_messages
+    Message.where('id > ?', last_message_id)
+  end
+end
---
+++ spec/services/messages_fetcher_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe MessagesFetcher do
+  context 'first time frontend call' do
+    it 'returns recent messages' do
+      message = create_message
+      last_message_id = nil
+
+      expect(MessagesFetcher.new(last_message_id).call).to eq([message])
+    end
+  end
+
+  context 'with last_message_id' do
+    it 'returns only messages that were created after the one with said id' do
+      first_message = create_message
+      second_message = create_message
+
+      expect(MessagesFetcher.new(first_message.id).call).to eq([second_message])
+    end
+  end
+end
+
+def create_message
+  Message.create
+end

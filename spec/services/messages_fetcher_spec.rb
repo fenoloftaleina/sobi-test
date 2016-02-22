ace32993b8d8bdbc347f2f94682b55989b975d92
Dominik Bylica <byldominik+fenoloftaleina@gmail.com>
2016-02-22 21:17:23 +0100

    Fix spec.

--- spec/services/messages_fetcher_spec.rb
+++ spec/services/messages_fetcher_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
 describe MessagesFetcher do
-  context 'first time frontend call' do
-    it 'returns recent messages' do
-      message = create_message
-      last_message_id = nil
+  describe '#call' do
+    context 'first time frontend call' do
+      it 'returns recent messages' do
+        message = create_message
+        last_message_id = nil
 
-      expect(MessagesFetcher.new(last_message_id).call).to eq([message])
+        expect(MessagesFetcher.new(last_message_id).call).to eq([message])
+      end
     end
-  end
 
-  context 'with last_message_id' do
-    it 'returns only messages that were created after the one with said id' do
-      first_message = create_message
-      second_message = create_message
+    context 'with last_message_id' do
+      it 'returns only messages that were created after the one with said id' do
+        first_message = create_message
+        second_message = create_message
 
-      expect(MessagesFetcher.new(first_message.id).call).to eq([second_message])
+        expect(MessagesFetcher.new(first_message.id).call).to eq([second_message])
+      end
     end
   end
 end

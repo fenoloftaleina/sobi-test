5c947fabe72fe268273a1bc7fd0cb3b09537d431
Dominik Bylica <byldominik+fenoloftaleina@gmail.com>
2016-02-23 01:26:56 +0100

    Revert "Add http caching."
    This reverts commit 465954dd645fc67e54649f5e4c408bca18a0d20b.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,7 @@
 class MessagesController < ApplicationController
   def index
-    if stale?(Message.last)
-      render json: MessagesSerializer.new(
-        MessagesFetcher.new(params[:last_message_id])
-      ).call, status: 200
-    end
+    render json: MessagesSerializer.new(
+      MessagesFetcher.new(params[:last_message_id])
+    ).call, status: 200
   end
 end

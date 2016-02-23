465954dd645fc67e54649f5e4c408bca18a0d20b
Dominik Bylica <byldominik+fenoloftaleina@gmail.com>
2016-02-23 01:26:51 +0100

    Add http caching.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,9 @@
 class MessagesController < ApplicationController
   def index
-    render json: MessagesSerializer.new(
-      MessagesFetcher.new(params[:last_message_id])
-    ).call, status: 200
+    if stale?(Message.last)
+      render json: MessagesSerializer.new(
+        MessagesFetcher.new(params[:last_message_id])
+      ).call, status: 200
+    end
   end
 end
